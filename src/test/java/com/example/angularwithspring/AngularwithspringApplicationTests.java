ce41838081f6cace59a6d42c53b8b385fc202885
NIIT <Shikhar.Dixit@stackroute.in>
2021-07-29 11:47:11 +0530

    done

---
+++ src/test/java/com/example/angularwithspring/AngularwithspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularwithspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
